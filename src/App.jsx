5aa814c18f636011a1507b34f9610d8af61f839d
rahul bora <rahul.bora@yesmadam.com>
2023-03-06 18:57:13 +0530

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
   Feedbacks,
   Hero,
   Navbar,
+  StarsCanvas,
   Tech,
   Works,
 } from "./components";
@@ -18,6 +19,16 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
