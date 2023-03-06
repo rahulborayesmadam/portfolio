c91c8032a50b50e6b5a8f1748a6c0907cde49160
rahul bora <rahul.bora@yesmadam.com>
2023-03-06 18:43:00 +0530

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from "./components";
 
 const App = () => {
-  return <div>3D Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
